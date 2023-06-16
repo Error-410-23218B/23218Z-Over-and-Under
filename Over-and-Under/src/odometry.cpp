d35a8cd55e91d315ff58361764045206b977af69
Error-410-23218B <19rjose@thelangton.org.uk>
2023-06-16 22:07:52 +0100

    Added Comments for tracking algorithm

--- Over-and-Under/src/odometry.cpp
+++ Over-and-Under/src/odometry.cpp
@@ -4,91 +4,76 @@
 #include <array>
 using namespace std;
 
-//add class here for all 3 encoders.
-/*
-int trackingAlgorithm(){
-float prevEncoderVal;
-float encoderDegL = FrontEncoderA.get();
-float encoderDegR = FrontEncoderB.get();
-float encoderValL = 2 * M_PI * encoderDegL;
-float encoderValR = 2 * M_PI * encoderDegR;
-float encoderValDeltaL   = encoderValL - prevEncoderVal;
- prevEncoderVal = encoderValL;
+const float sL = 5.0;
+const float sR = 5.0;
 
-
-
-return 0;
-}
-*/
-//Objects
-const float sL= 5.0;
-const float sR= 5.0;
-
-
-
-
-//The class is here.
-class Odometry{
+class Encoding
+{
 public:
     float encoderDeg;
-    float encoderTravel =   2 * M_PI * encoderDelta; 
+    float encoderTravel = 2 * M_PI * encoderDelta;
     float encoderDelta;
     float prevEncoder;
-    Odometry(float encoder){
+    Encoding(float encoder)
+    {
 
         encoderDeg = encoder;
-        encoderDelta = encoderDeg - prevEncoder; 
+        encoderDelta = encoderDeg - prevEncoder;
         prevEncoder = encoderDeg;
-
-
     }
 };
 
+// Tracking Algorithm, calculates absoulute position of robot on field.
+void tracking()
+{
+    // Vectors, first index is x value, second index is y value.
+    array<double, 2> globalOffset;
+    array<double, 2> localOffset;
+    array<double, 2> prevGlobalOffset;
+    array<double, 2> absoulutePosition;
 
-void tracking(){
-array<double,2> globalOffset;
-array<double,2>localOffset;
-array<double,2>prevGlobalOffset;
-array<double,2>absoulutePosition;
-float prevAbsOrientation;
-float prevEncoderDelta;
-float encoderDistanceDelta = EncoderLeft.encoderTravel + EncoderRight.encoderTravel;
-float absOrientation = encoderDistanceDelta + ((EncoderLeft.encoderTravel - EncoderRight.encoderTravel)/(sL+sR));
-float deltaAbsOrientation = absOrientation - prevAbsOrientation;
-prevAbsOrientation = absOrientation;
-float averageOrientation;
-float localOffset = 2 * sin(deltaAbsOrientation/2);
+    float prevAbsOrientation;
+    float prevEncoderDelta;
+    float encoderDistanceDelta = EncoderLeft.encoderTravel + EncoderRight.encoderTravel;
+    float absOrientation = encoderDistanceDelta + ((EncoderLeft.encoderTravel - EncoderRight.encoderTravel) / (sL + sR));
+    float deltaAbsOrientation = absOrientation - prevAbsOrientation;
+    prevAbsOrientation = absOrientation;
+    float averageOrientation;
+    float localOffset = 2 * sin(deltaAbsOrientation / 2);
 
+    // If the left side distance change = right side distance change.
 
-if(deltaAbsOrientation == 0){
+    if (deltaAbsOrientation == 0)
+    {
 
+        localOffset[0] = EncoderBack.encoderTravel;
+        localOffset[1] = EncoderRight.encoderTravel;
+    }
+    // else if left side distance change != right side distance change.
 
+    else
+    {
+        localOffset[0] = 2 * sin((deltaAbsOrientation / 2) * (EncoderLeft.encoderTravel / deltaAbsOrientation) + EncoderBack.encoderTravel);
+        localOffset[1] = (EncoderRight.encoderDeg / deltaAbsOrientation) + EncoderRight.encoderTravel;
+    }
 
+    averageOrientation = prevAbsOrientation + (deltaAbsOrientation / 2);
+
+    // Conversion of vectors from Cartesian Coordinates(Normal) to Polar Coordinates
+    globalOffset[0] = sqrt(pow(localOffset[0], 2) + pow(localOffset[1], 2)); // cartesian:x gets turned into polar: r
+    globalOffset[1] = atan(localOffset[1] / localOffset[1]);                 // cartesian:y gets turned into polar:θ
+    globalOffset[0] = -averageOrientation - (0.75 * M_PI);                   // Rotate globalOffset by the negative of the average orientation, by changing polar:r
+
+    // Conversion back of vectors from Polar Coordinates to Cartesian coordinates
+    globalOffset[0] = globalOffset[0] * cos(globalOffset[1]);
+    globalOffset[1] = globalOffset[0] * sin(globalOffset[1]);
+
+    for (int i = 0; i < 2; i++)
+        absoulutePosition[i] = prevGlobalOffset[i] + globalOffset[i];
+
+    prevGlobalOffset = globalOffset;
 }
-else{
-    localOffset[0] = 2 * sin((deltaAbsOrientation/2)* (EncoderLeft.encoderTravel/deltaAbsOrientation) +  EncoderBack.encoderTravel);
-    localOffset[1] = (EncoderRight.encoderDeg/deltaAbsOrientation) + EncoderRight.encoderTravel;
-}
 
- averageOrientation = prevAbsOrientation + (deltaAbsOrientation/2);
- 
- globalOffset[0] = sqrt(pow(localOffset[0],2) + pow(localOffset[1],2));
- globalOffset[1] = atan(localOffset[1]/localOffset[1]);
- globalOffset[0] = -averageOrientation - (0.75*M_PI);
-for (int i = 0; i<2;i++) absoulutePosition[i] = prevGlobalOffset[i] + globalOffset[i];
-
-prevGlobalOffset = globalOffset;
-
-
-
-
-}
-
-
-    
-
-
-Odometry EncoderLeft(1.0);
-Odometry EncoderRight(2.0);
-Odometry EncoderBack(3.0);
-
+Encoding EncoderLeft(1.0);
+Encoding EncoderRight(2.0);
+Encoding EncoderBack(3.0);
