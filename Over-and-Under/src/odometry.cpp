b6dc12c7764f35bae3e2897ebc94fae2e3b5344c
Langton-Vex <19rjose@thelangton.org.uk>
2023-06-30 16:45:20 +0100

    Custom Drivetrain Class

--- Over-and-Under/src/odometry.cpp
+++ Over-and-Under/src/odometry.cpp
@@ -29,11 +29,7 @@ const float sR= 5.0;
 
 //The class is here.
 class Odometry{
-public:
-    float encoderDeg;
-    float encoderTravel =   2 * M_PI * encoderDelta; 
-    float encoderDelta;
-    float prevEncoder;
+
     Odometry(float encoder){
 
         encoderDeg = encoder;
@@ -45,7 +41,7 @@ public:
 };
 
 
-void tracking(){
+int tracking(){
 array<double,2> globalOffset;
 array<double,2>localOffset;
 array<double,2>prevGlobalOffset;
@@ -61,7 +57,8 @@ float localOffset = 2 * sin(deltaAbsOrientation/2);
 
 
 if(deltaAbsOrientation == 0){
-
+localOffset[0] =  EncoderBack.encoderDeg;
+localOffset[1] = EncoderRight.encoderDeg;
 
 
 }
@@ -71,7 +68,6 @@ else{
 }
 
  averageOrientation = prevAbsOrientation + (deltaAbsOrientation/2);
- 
  globalOffset[0] = sqrt(pow(localOffset[0],2) + pow(localOffset[1],2));
  globalOffset[1] = atan(localOffset[1]/localOffset[1]);
  globalOffset[0] = -averageOrientation - (0.75*M_PI);
@@ -80,7 +76,7 @@ for (int i = 0; i<2;i++) absoulutePosition[i] = prevGlobalOffset[i] + globalOffs
 prevGlobalOffset = globalOffset;
 
 
-
+return 0;
 
 }
 
