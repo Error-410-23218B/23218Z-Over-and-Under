163271754e2f324a060c0c2ffc3ba3d3c75dc0ea
Langton-Vex <19rjose@thelangton.org.uk>
2023-07-04 16:16:38 +0100

    Update gui.cpp

--- Over-and-Under/src/gui.cpp
+++ Over-and-Under/src/gui.cpp
@@ -1,3 +1,22 @@
 #include "vex.h"
-#include <sstream>
+
+Slides[2][2]= {
+    {"mainmenu.png", false},
+    {"running.png", false}
+};
+
+void MainMenu(){
+    Brain.Screen.Display(Slides[0])
+    Slides[0][1] = true;
+    return;
+}
+
+void GUI_Handler(){
+    
+}
+
+int main(){
+    MainMenu;
+    GUI_Handler;
+}
 
