fd3592180942361d37e5d9dfb273709a2860d824
Error-410-Sub <116311099+Error-410-Sub@users.noreply.github.com>
2023-07-05 15:59:00 +0100

    broke rohan's beautiful code

--- Over-and-Under/src/gui.cpp
+++ Over-and-Under/src/gui.cpp
@@ -3,56 +3,11 @@
 #include <map>
 std::map<std::string,bool> slidesMap;
 
-/* Use classes for the GUI, e.g button class, maybe nested?, as a private classs
- class GUI{
-    private: // this could be protected too, if you need it to be, probably not, this ain't an API
-            class Buttons{
-                protected:
-                        blah blah
-            }
-
-            class Screen{
-                protected:
-                        Blah Blah
-            }
-
- }
- */
-
-
-//excuse me wtf?, why declare as a 2d array, when it is a hashmap
-//This is awful
 /*
-//This isn't python, what type is it?
-Slides[2][2]= {
-    {"mainmenu.png", false},
-    {"running.png", false}
-};
-*/
-
-
-
-void MainMenu(){
-    //How did you screw up method calls?
-    Brain.Screen.render();
-    //2d array = hashmap? wtf?
-    // fixed your code.
     slidesMap.insert({"mainmenu.png",false});
     slidesMap.insert({"running.png",false});
-    slidesMap["running.png"] = true;
-    // why the empty return? It's a void function
-    //return;
-}
+*/
 
-void GUI_Handler(){
-    //empty function why?
+class GUI(){
+    
 }
-//WTF is there a definition of main here, main is the calling point for the whole program? wtf?
-void GUI(){
-
-    //TF are you doing, where are the brackets?
-    MainMenu();//<----
-    GUI_Handler();//<-----
-    //Why did you declare it as an int function, and no return?
-}
-
