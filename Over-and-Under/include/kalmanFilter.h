1fe7b5ee1de0b446ce33575bc2f7e6a58c6e419c
Langton-Vex <19rjose@thelangton.org.uk>
2023-07-20 10:02:13 +0100

    Kalman Restructure

--- Over-and-Under/include/kalmanFilter.h
+++ Over-and-Under/include/kalmanFilter.h
@@ -1,50 +1,40 @@
-<<<<<<< HEAD
-
-=======
 //kalmanFilter.h
->>>>>>> 30893a4c405289280efb41710489d032eb29b238
+#pragma once
+#ifndef kalmanFilter_h
+#define kalmanFilter_h
+
+
+struct kalmanFilter
+{
+    double velocity;
+    double pos;
+    double var;
+}prev_state;
+
+
+
 namespace eftl{
-    struct kalmanFilterVar
+    class kalmanFilter
     {
-        double velocity;
-        double pos;
-        double var;
-
-    }prevState,predict_state
-    
-    
-    class kalmanFilter{
-        public:
-                double variance;
-                double new_states;
-                double prev_states;
-                double y;
-                double K;
-                double predict_states;
-                double pos;
-                double var;
-                double state;
-                double pos;
-                double var;
-                double state;
-                const double process_var = 0.1;
-                const double measurement_var = 0.1;
-                const double time_step = 0.01;
-                double dx;
-                double gauss1;
-                double gauss2;
-                double residual;
-                double kalman_gain;
-                double new_var;
-
-             
- 
-                double predict();
-                double update();
-                void kalmanIterate();
-                double multiply(double gauss3,double gauss4);
-                
-
+    public:
+        double measurement;
+        double state;
+        double PROCESS_VAR = 0.1;
+        double MEASUREMENT_VAR = 0.1;
+        double TIME_STEP = 0.01;
+        double new_state;
+        double new_var;
+        double predict_state;
+        double predict_var;
+        double kalman_gain;
+        double residual;
+        kalmanFilter();
+        double predict();
+        double update();
+        double multiply(double gauss,double gauss2);
+      
     };
+   
 }
 
+#endif
--- Over-and-Under/src/kalmanFilter.cpp
+++ Over-and-Under/src/kalmanFilter.cpp
@@ -1,46 +1,32 @@
 #include "vex.h"
+#include "tgmath.h"
 #include "kalmanFilter.h"
 
-using namespace vex;
 
-double eftl::kalmanFilter::predict(){
-    state = F * prevState
 
-    dx = prevState.velocity * time_step;
-    pos = prevState.pos +dx;
-    var = prevState.var + process_var;
-    return pos,var;
-
+eftl::kalmanFilter::kalmanFilter() {
+    predict_state,predict_var =  predict();
+    new_state,new_var = update();
 }
 
+eftl::kalmanFilter::predict(){
+    double dx = prev_state.velocity * TIME_STEP;
+    double pos = prev_state.pos + dx;
+    double var = prev_state.var + PROCESS_VAR;
+    return pos,var
+}
 
-double eftl::kalmanFilter::update(){
-    
+eftl::kalmanFilter::update(){
     residual = measurement - state;
-    kalman_gain =  var/(var+measurement_var);
-    new_state = state + kalman_gain;
-    new_var = (1- kalman_gain) * state.var;
-    return new_state,new_var
+    kalman_gain = var / (var + MEASUREMENT_VAR);
+    new_state = state + kalman_gain
+    new_var = (1- kalman_gain) * var
 }
 
-void eftl::kalmanFilter::kalmanIterate(){
-    for (int time_step;time_step < 20; time_step++){
-        predict_state,predict_state.var= predict();
-        new_state,new_state.var = update();
-    }
-
-
-}
-
-double eftl::kalmanFilter::multiply(){
-    double mean1,covariance1 = gauss1;
+eftl::kalmanFilter::multiply(double gauss,double gauss2){
+    double mean1,covariance1 = gauss;
     double mean2,covariance2 = gauss2;
-
-    double mean = pow(covariance2 * (covariance1 +covariance2),-1)*mean1 + covariance1 * pow((covariance1 + covariance2),-1) * mean2;
-    double covariance = covariance1 * pow(( covariance1 + covariance2),-1) * covariance2;
-
-    return mean,covariance;  
+    double mean = covariance2 * pow((covariance1 + covariance2),-1) * mean1 + covariance1 * pow((covariance1 + covariance2),-1) * mean2;
+    double covariance = covariance1 * pow((covariance1 + covariance2),-1) * covariance2;
+    return mean,covariance 
 }
-
-
-
