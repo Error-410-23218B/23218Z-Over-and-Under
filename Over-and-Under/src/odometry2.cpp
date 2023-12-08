0f5b99be81b56f5beffe2c564d73e3e29359bf7c
Langton-Vex <19rjose@thelangton.org.uk>
2023-12-08 17:02:34 +0000

    ddfsdfdsdsfd

--- Over-and-Under/src/odometry2.cpp
+++ Over-and-Under/src/odometry2.cpp
@@ -35,7 +35,7 @@ Encoder operator-(const Encoder& other) const
         };
     }
 
-    Encoder operator+(const Encoder& other) const
+Encoder operator+(const Encoder& other) const
     {
         return {
             leftEncoder + other.leftEncoder,
@@ -72,44 +72,47 @@ Encoder reset;
 Encoder wheelDistance;
 Encoder lastResetDistance;
 Encoder skid;
+Encoder desired; 
+
 
 
 private:
-double leftToCenter;
-double rightToCenter;
-double backToCenter;
-double deltaOrientation;
-double wheelRadius;
+    double leftToCenter;
+    double rightToCenter;
+    double backToCenter;
+    double deltaOrientation;
+    double wheelRadius;
 
-double absouluteOrientation;
-double prevGlobalOrientation;
-double prevGlobalOrientationAtLastReset;
-double changeInGlobalOrientation;
-double averageOrientation;
+    double absouluteOrientation;
+    double prevGlobalOrientation;
+    double prevGlobalOrientationAtLastReset;
+    double changeInGlobalOrientation;
+    double averageOrientation;
 
 
-std::vector<double> prevGlobalPositionVector;
-double localOffset[2];
-double globalOffset[2];
-double absoulutePosition[2];
-double prevAbsoulutePosition[2];
+    std::vector<double> prevGlobalPositionVector;
+    double localOffset[2];
+    double globalOffset[2];
+    double absoulutePosition[2];
+    double prevAbsoulutePosition[2];
 public:
     odometry2(double wheel_radius);
     ~odometry2();
     void step(Encoder encoderVal);
+    void reset();
 };
 
-odometry2::odometry2(double wheel_radius): wheelRadius(wheel_radius)
-{
 
-}
+
+odometry2::odometry2(double wheel_radius): wheelRadius(wheel_radius)
+{}
 
 odometry2::~odometry2()
-{
-}
+{}
+
+odometry2::reset(){}
 
 inline void odometry2::step(Encoder encoderVal){
-
     difference = encoderVal - prev;
     wheelDistance = (difference.radToDeg()/360) * (2 * M_PI * wheelRadius);
     deltaOrientation = (difference.leftEncoder - difference.rightEncoder)/(leftToCenter+rightToCenter);
@@ -117,6 +120,7 @@ inline void odometry2::step(Encoder encoderVal){
     lastResetDistance = ((skid - encoderVal).radToDeg()/360) * (2 * M_PI * wheelRadius);
     absouluteOrientation = prevGlobalOrientationAtLastReset + (lastResetDistance.leftEncoder - lastResetDistance.rightEncoder) / (leftToCenter + rightToCenter);
     changeInGlobalOrientation = absouluteOrientation - prevGlobalOrientation;
+
     if (changeInGlobalOrientation == 0)
     {
         localOffset[0],localOffset[1] = difference.backEncoder,difference.rightEncoder;
@@ -124,16 +128,12 @@ inline void odometry2::step(Encoder encoderVal){
     else{
         localOffset[0],localOffset[1] = (difference.backEncode  r/changeInGlobalOrientation) + backToCenter, (difference.rightEncoder/changeInGlobalOrientation)+rightToCenter;
         localOffset[0],localOffset[1] *= (2 * sin(absouluteOrientation/2));
-    }
-    
-
+}
     averageOrientation = prevGlobalOrientation + (changeInGlobalOrientation/2);
     double localOffsetPolar[2] = {hypot(localOffset[0],localOffset[1]), atan(((double)localOffset[1]/localOffset[0]))};
     localOffset[1] -= averageOrientation;
     globalOffset[0],globalOffset[1] = localOffsetPolar[0] * cos(localOffset[1]),localOffsetPolar[0] * sin(localOffset[1]);
-    
     absoulutePosition[0],absoulutePosition[1] = prevAbsoulutePosition[0] + globalOffset[0], prevAbsoulutePosition[1] + globalOffset[1];
     prevAbsoulutePosition[0],prevAbsoulutePosition[1] = absoulutePosition[0],absoulutePosition[1];
-    
 }
 
