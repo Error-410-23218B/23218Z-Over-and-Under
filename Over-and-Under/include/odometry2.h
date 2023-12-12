e5a0c3c635a42fa878b537a4ad955945da9ef989
Langton-Vex <19rjose@thelangton.org.uk>
2023-12-12 14:43:19 +0000

    sdfsdsfsdf

---
+++ Over-and-Under/include/odometry2.h
@@ -0,0 +1,107 @@
+#define _USE_MATH_DEFINES
+#include "vex.h"
+#include <vector>
+#include <cmath>
+
+
+namespace eftl
+{
+class odometry2
+{
+protected:
+struct Encoder
+        {
+            double leftEncoder;
+            double rightEncoder;
+            double backEncoder;
+
+
+
+Encoder radToDeg() const{
+    return {
+        leftEncoder * 180/M_PI,
+        rightEncoder * 180/M_PI,
+        backEncoder * 180/M_PI
+    };
+}
+
+
+Encoder operator-(const Encoder& other) const
+    {
+        return {
+            leftEncoder - other.leftEncoder,
+            rightEncoder - other.rightEncoder,
+            backEncoder - other.backEncoder
+        };
+    }
+
+Encoder operator+(const Encoder& other) const
+    {
+        return {
+            leftEncoder + other.leftEncoder,
+            rightEncoder + other.rightEncoder,
+            backEncoder + other.backEncoder
+        };
+    }
+
+      Encoder operator*(double operand) const
+    {
+        return {
+            leftEncoder * operand,
+            rightEncoder * operand,
+            backEncoder * operand
+        };
+    }
+
+       Encoder operator/(double operand) const
+    {
+        return {
+            leftEncoder / operand,
+            rightEncoder / operand,
+            backEncoder / operand
+        };
+    }
+
+
+
+};
+
+Encoder current;
+Encoder prev;
+Encoder difference;
+Encoder reset;
+Encoder wheelDistance;
+Encoder lastResetDistance;
+Encoder skid;
+Encoder desired; 
+
+
+
+private:
+    double leftToCenter;
+    double rightToCenter;
+    double backToCenter;
+    double deltaOrientation;
+    double wheelRadius;
+
+    double absouluteOrientation;
+    double prevGlobalOrientation;
+    double prevGlobalOrientationAtLastReset;
+    double changeInGlobalOrientation;
+    double averageOrientation;
+
+
+    std::vector<double> prevGlobalPositionVector;
+    double localOffset[2];
+    double globalOffset[2];
+    double absoulutePosition[2];
+    double prevAbsoulutePosition[2];
+public:
+    odometry2(double wheel_radius);
+    ~odometry2();
+    void step(Encoder encoderVal);
+    void reset(Encoder encoderReset);
+    void reset(Encoder encoderReset,double angle);
+
+};
+}
--- Over-and-Under/src/odometry2.cpp
+++ Over-and-Under/src/odometry2.cpp
@@ -2,106 +2,10 @@
 #include "vex.h"
 #include <vector>
 #include <cmath>
+#include "odometry2.h"
 
 
 
-
-class odometry2
-{
-protected:
-        struct Encoder
-        {
-            double leftEncoder;
-            double rightEncoder;
-            double backEncoder;
-
-
-
-Encoder radToDeg() const{
-    return {
-        leftEncoder * 180/M_PI,
-        rightEncoder * 180/M_PI,
-        backEncoder * 180/M_PI
-    };
-}
-
-
-Encoder operator-(const Encoder& other) const
-    {
-        return {
-            leftEncoder - other.leftEncoder,
-            rightEncoder - other.rightEncoder,
-            backEncoder - other.backEncoder
-        };
-    }
-
-Encoder operator+(const Encoder& other) const
-    {
-        return {
-            leftEncoder + other.leftEncoder,
-            rightEncoder + other.rightEncoder,
-            backEncoder + other.backEncoder
-        };
-    }
-
-      Encoder operator*(double operand) const
-    {
-        return {
-            leftEncoder * operand,
-            rightEncoder * operand,
-            backEncoder * operand
-        };
-    }
-
-       Encoder operator/(double operand) const
-    {
-        return {
-            leftEncoder / operand,
-            rightEncoder / operand,
-            backEncoder / operand
-        };
-    }
-
-
-
-};
-
-Encoder current;
-Encoder prev;
-Encoder difference;
-Encoder reset;
-Encoder wheelDistance;
-Encoder lastResetDistance;
-Encoder skid;
-Encoder desired; 
-
-
-
-private:
-    double leftToCenter;
-    d ouble rightToCenter;
-    double backToCenter;
-    double deltaOrientation;
-    double wheelRadius;
-
-    double absouluteOrientation;
-    double prevGlobalOrientation;
-    double prevGlobalOrientationAtLastReset;
-    double changeInGlobalOrientation;
-    double averageOrientation;
-
-
-    std::vector<double> prevGlobalPositionVector;
-    double localOffset[2];
-    double globalOffset[2];
-    double absoulutePosition[2];
-    double prevAbsoulutePosition[2];
-public:
-    odometry2(double wheel_radius);
-    ~odometry2();
-    void step(Encoder encoderVal);
-    void reset();
-};
 
 
 
@@ -111,10 +15,14 @@ odometry2::odometry2(double wheel_radius): wheelRadius(wheel_radius)
 odometry2::~odometry2()
 {}
 
-odometry2::reset(){
-
-
+odometry2::reset(Encoder encoderReset){
+    current = encoderReset;
 }
+
+ void reset(Encoder encoderReset,double angle){
+    reset(encoderReset);
+    
+ }
 
 inline  odometry2::step(Encoder encoderVal){
     difference = encoderVal - prev;
@@ -126,14 +34,16 @@ inline  odometry2::step(Encoder encoderVal){
     changeInGlobalOrientation = absouluteOrientation - prevGlobalOrientation;
 
     if (changeInGlobalOrientation == 0)
-        {
+{
             localOffset[0],localOffset[1] = difference.backEncoder,difference.rightEncoder;
-        }
+}
+
     else
     {
             localOffset[0],localOffset[1] = (difference.backEncode  r/changeInGlobalOrientation) + backToCenter, (difference.rightEncoder/changeInGlobalOrientation)+rightToCenter;
             localOffset[0],localOffset[1] *= (2 * sin(absouluteOrientation/2));
     }
+
     averageOrientation = prevGlobalOrientation + (changeInGlobalOrientation/2);
     double localOffsetPolar[2] = {hypot(localOffset[0],localOffset[1]), atan(((double)localOffset[1]/localOffset[0]))};
     localOffset[1] -= averageOrientation;
